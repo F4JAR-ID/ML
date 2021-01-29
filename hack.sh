530edd0a057a684faf2742491a869bf417a48d9a
Fajar Septianto <49784833+F4JAR-ID@users.noreply.github.com>
2021-01-29 10:38:40 +0700

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -21,17 +21,17 @@ echo $i "▀░░ ▀░░▀ █▄█ ▀░░▀ ▀░▀▀   ▀▀▀ 
 
 
 sleep 3
-echo "                                                          >
+echo "                                                          "
  echo $i"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
  echo $ku"      [•]  INDONESIAN CYBER      [•]      "
  echo $ku"      [•]  AUTHOR: Jarr Id       [•] "
- echo $ku"     【✔】      We Lose【✔】       "
+ echo $ku"   【✔】         We Lose            【✔】       "
  echo $me" "
  echo $i"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
  sleep 1
- echo "                                                          >
+ echo "                                                       "
 sleep 2
-echo "                                                          >
+echo "                                                         "
 echo $me"               ╭∩╮（￣▽￣）╭∩╮"
 echo $ku"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
 echo $i"        [•]     [1] Diamond           [•]      "
@@ -43,5 +43,34 @@ echo $i"        [•]     [6]EXIT               [•]    "
 echo $ku"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
 sleep 3
  echo $pu"               [√]PILIH NOMOR :          "
+read nomor
+ if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+echo $ku"TUNGGU SEBENTAR EA NGAB"
+sleep 2
+echo $1"■ Sedang Loading••• 5℅ "
+sleep 2
+echo $1"■ Sedang Loading••• 11℅"
+sleep 2
+echo $1"■ Sedang Loading••• 17℅"
 sleep 3
- echo 
+echo $1"■ Sedang Loading••• 28℅"
+sleep 1
+echo $1"■ Sedang Loading••• 34℅"
+sleep 2
+echo $1"■ Sedang Loading••• 49℅"
+sleep 1
+echo $1"■ Sedang Loading••• 68℅"
+sleep 3
+echo $1"■ Sedang Loading••• 74℅"
+sleep 2
+echo $1"■ Sedang Loading••• 85℅"
+sleep 1
+echo $1"■ Sedang Loading••• 97℅"
+sleep 2
+echo $1"■ Sedang Loading... 100℅"
+sleep 3
+echo $i" ________HASILNYA NIH NGAB________ "
+
+

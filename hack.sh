ce8b55e3430ff3a0a0d7a4e6bd3192c386f0e439
Fajar Septianto <49784833+F4JAR-ID@users.noreply.github.com>
2021-01-29 09:54:00 +0700

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,47 @@
+
+	#!/usr/bin/bash
+
+ bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+sleep 5
+echo $ku"█████████                      [•]PREMIUM"
+echo $ku"█▄█████▄█      *╔═══❖•ೋ° °ೋ•❖═══╗*"
+echo $ku"█▼▼▼▼▼"
+echo $ku"█                      HACKER      "
+echo $me"█▲▲▲▲▲"
+echo $me"█████████      *╚═══❖•ೋ° °ೋ•❖═══╝*"
+echo $bi"██ ██"
+echo $ku"╭───────╯•╰───────╮ "
+echo $cy"  ╔╗╔╗╔══╗╔═╗╔╦╗  "
+echo $cy"  ║╚╝║║╔╗║║╔╝║╔╝  "
+echo $cy"  ║╔╗║║╠╣║║╚╗║╚╗  "
+echo $cy"  ╚╝╚╝╚╝╚╝╚═╝╚╩╝  "
+echo $i" ────────────────── "
+sleep 2
+echo "                                                          >
+ echo $i"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
+ echo $ku"      [•]  INDONESIAN CYBER      [•]      "
+ echo $ku"      [•]  AUTHOR: Jarr Id       [•] "
+ echo $ku"     【✔】      We Lose【✔】       "
+ echo $me" "
+ echo $i"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
+ sleep 1
+ echo "                                                          >
+sleep 2
+echo "                                                          >
+echo $me"               ╭∩╮（￣▽￣）╭∩╮"
+echo $ku"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
+echo $i"        [•]     [1] Diamond           [•]      "
+echo $i"        [•]     [2] Gift skin         [•]    "
+echo $i"        [•]     [3] Battel Point      [•]    "
+echo $i"        [•]     [4] Hack Via Gmail    [•]    "
+echo $i"        [•]     [5] Hack Via Montoon  [•]    "
+echo $i"        [•]     [6]EXIT               [•]    "
+echo $ku"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
+sleep 3
+ echo $pu"               [√]PILIH NOMOR :          "
